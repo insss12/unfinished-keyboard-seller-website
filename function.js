496a8d8ead9f2dc1005aa86aacf6e9b22edf8328
insss <thanh84.ptit@gmail.com>
2025-11-04 15:22:26 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+const home = document.getElementById("home");
+const products = document.getElementById("products");
+const cart = document.getElementById("cart");
+const help = document.getElementById("help");
+const button = document.getElementById("real")
+
+button.addEventListener("click", () => {
+    window.location.href=""
+})
